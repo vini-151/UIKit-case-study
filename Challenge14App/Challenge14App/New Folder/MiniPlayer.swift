014bc8a07f913fbd4cce573fe13cb43c27ffe828
Vini <ovini.15515@gmail.com>
2026-05-19 16:59:07 -0300

    miniPlayer da Le

---
+++ Challenge14App/Challenge14App/New Folder/MiniPlayer.swift
@@ -0,0 +1,58 @@
+//  MiniPlayer.swift
+//  Challenge14App
+//
+//  Created by Letícia Delmilio Soares on 19/05/26.
+//
+import SwiftUI
+
+struct MiniPlayer: View {
+    var body: some View {
+
+        HStack {
+            HStack(alignment: .center, spacing: 20){
+                Image("Album")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Rectangle())
+                    .border(Color(.gray), width: 2)
+                    .frame(width: 60)
+
+
+                Text("Forever Outsider")
+                    .font(Font.body.bold())
+                    .multilineTextAlignment(.center)
+                    .lineLimit(2)
+            }
+            .padding(.leading)
+
+            Spacer()
+
+            HStack(spacing: 20){
+                Button{
+                }label: {
+                    Image(systemName: "play.fill")
+                        .font(.title)
+                }
+                .foregroundStyle(Color(.white))
+                Button{
+                }label: {
+                    Image(systemName: "forward.fill")
+                        .font(.title)
+                }
+                .foregroundStyle(Color(.white))
+
+            }.padding(30)
+
+
+
+        } .background(RoundedRectangle(cornerRadius: 10).fill(.btnBg))
+//            .padding(.horizontal, 10)
+
+
+        }
+
+
+    }
+#Preview {
+    MiniPlayer()
+}
